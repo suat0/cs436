98d8982555fd10ef2beed943dd93dee0e3f56341
dilansatalgl <satalogludilan@gmail.com>
2025-03-13 13:39:13 +0300

    Set up react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
+import AppRoutes from "./routes";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1>My React App</h1>
+      
+      <AppRoutes />
     </div>
   );
 }
-
-export default App;
-
+export default App; 
---
+++ frontend/src/pages/LoginSignup.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+function LoginSignup() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Login Signup</h1>
+    </div>
+  );
+}
+
+export default LoginSignup;
