c0ee5e5de1f4e192d4dc2de2c670e12e762ef27c
dilansatalgl <satalogludilan@gmail.com>
2025-03-27 18:17:40 +0300

    fix syntax and css issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,32 @@ import Home from "./pages/Home";
 import LoginSignup from "./pages/LoginSignup";
 import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
+import Comment from  "./pages/Comment";
 
 function App() {
+  const appContainerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  };
+
+  const mainContentStyle = {
+    flex: 1, 
+  };
+
   return (
-    <>
+    <div style={appContainerStyle}>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/account" element={<LoginSignup />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/comments" element={<Comment />} />
-      </Routes>
+      <div style={mainContentStyle}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/account" element={<LoginSignup />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/comments" element={<Comment />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- frontend/src/pages/Comment.jsx
+++ frontend/src/pages/Comment.jsx
@@ -1,6 +1,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-return (
-      <h2>Comments page </h2>
+
+function Comment() {
+  return (
+    <div style={{ marginTop: "80px", padding: "20px" }}>
+      <h2>Comments page</h2>
+    </div>
+    
   );
+}
+
+export default Comment;
+
