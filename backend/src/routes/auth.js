240963542d40d9c6e42694342c923e00ad92e3d7
alpay9 <alpay9393@gmail.com>
2025-04-03 23:41:55 +0300

    fix path

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const db = require("../config"); 
+const db = require("../controllers/db"); 
 const rateLimit = require("express-rate-limit");
 
 
